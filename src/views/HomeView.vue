f6dc8148f60b09d1649ef88db25a8dde6ee8ec1e
rlanrid <rladnrider77@gmail.com>
2023-10-30 13:14:16 +0900

    2023-10-30 수업내용추가

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "@/components/Header.vue"
+import Slider from "@/components/Slider.vue"
+import Image from "@/components/Image.vue"
+import ImgText from "@/components/ImgText.vue"
+import Card from "@/components/Card.vue"
+import Banner from "@/components/Banner.vue"
+import Text from "@/components/Text.vue"
+import Footer from "@/components/Footer.vue"
+</script>
+
+<template>
+  <Header />
+  <main>
+    <Slider />
+    <Image />
+    <ImgText />
+    <Card />
+    <Banner />
+    <Text />
+  </main>
+  <Footer />
+</template>
